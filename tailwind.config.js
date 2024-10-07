4c5e3615aec51772183f55bc9991859cf2fc881c
Md Tanvir <mdtanvircse572@gmail.com>
2024-10-07 17:54:54 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Scan all HTML and JS files in the current folder and src folder
+  content: ["./src/**/*.{html,js}", "./**/*.{html,js}"],
+  theme: {
+      extend: {
+          // Example of extending theme with custom colors
+          colors: {
+              customGreen: '#10B981',
+              customRed: '#EF4444',
+          },
+      },
+  },
+  plugins: [],
+}
